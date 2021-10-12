e3eb6144ec4315068548a3ae5eeff1659918e05d
Edmilson <scovinowork@gmail.com>
2021-10-12 10:38:19 -0300

    falta dois componentes

--- app/src/app/listar-simbolos/listar-simbolos.component.ts
+++ app/src/app/listar-simbolos/listar-simbolos.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {DataServiceService} from "./data-service.service";
 import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
 
 @Component({
   selector: 'app-listar-simbolos',
@@ -11,6 +13,9 @@ export class ListarSimbolosComponent implements OnInit {
 
   displayedColumns = ['userId', 'id', 'title'];
   dataSource!:MatTableDataSource<any>;
+
+  @ViewChild('paginator') paginator! : MatPaginator;
+  @ViewChild(MatSort) matSort! : MatSort;
   constructor(private service: DataServiceService) {
 
   }
@@ -18,8 +23,13 @@ export class ListarSimbolosComponent implements OnInit {
   ngOnInit(): void {
     this.service.getUserData().subscribe((response: any) => {
       this.dataSource = new MatTableDataSource(response)
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.matSort;
       console.log('response is', response);
     })
+  }
+  filterData($event : any){
+    this.dataSource.filter = $event.target.value;
   }
 
 }
