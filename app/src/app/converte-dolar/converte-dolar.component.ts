4d9ac689cad6b9c8ca0dde4e5234b5856a41c1ce
Edmilson <scovinowork@gmail.com>
2021-10-13 13:52:06 -0300

    tabelas do converte Dolar E Conversor Moedas

--- app/src/app/converte-dolar/converte-dolar.component.ts
+++ app/src/app/converte-dolar/converte-dolar.component.ts
@@ -1,71 +1,39 @@
-import {AfterViewInit, Component, ViewChild} from "@angular/core";
+import {Component, OnInit, ViewChild} from "@angular/core";
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
+import {ConverteDolarService} from "./converte-dolar.service";
 
 
-export interface UserData {
-  id: string;
-  name: string;
-  progress: string;
-  fruit: string;
-}
-/** Constants used to fill up our data base. */
-const FRUITS: string[] = [
-  'blueberry', 'lychee', 'kiwi', 'mango', 'peach', 'lime', 'pomegranate', 'pineapple'
-];
-const NAMES: string[] = [
-  'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
-  'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
-];
+
 @Component({
   selector: 'app-converte-dolar',
   templateUrl: './converte-dolar.component.html',
   styleUrls: ['./converte-dolar.component.css']
 })
 
-export class ConverteDolarComponent implements AfterViewInit {
+export class ConverteDolarComponent implements OnInit {
 
-  displayedColumns: string[] = ['id', 'name', 'progress', 'fruit'];
-  dataSource: MatTableDataSource<UserData>;
+  displayedColumns = ['userId', 'id', 'title'];
+  dataSource!:MatTableDataSource<any>;
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild('paginator') paginator! : MatPaginator;
+  @ViewChild(MatSort) matSort! : MatSort;
+  constructor(private service: ConverteDolarService) {
 
-  constructor() {
-    // Create 100 users
-    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
-
-    // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
   }
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+  ngOnInit(): void {
+    this.service.getUserData().subscribe((response: any) => {
+      this.dataSource = new MatTableDataSource(response)
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.matSort;
+      console.log('response is', response);
+    })
   }
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
+  filterData($event : any){
+    this.dataSource.filter = $event.target.value;
   }
 }
 
-/** Builds and returns a new User. */
-function createNewUser(id: number): UserData {
-  const name = NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
-
-  return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))]
-  };
-
-}
 
--- app/src/app/converte-moedas/converte-moedas.component.ts
+++ app/src/app/converte-moedas/converte-moedas.component.ts
@@ -1,68 +1,36 @@
-import {AfterViewInit, Component, ViewChild} from "@angular/core";
+import {Component, OnInit, ViewChild} from "@angular/core";
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
+import {DataServiceService} from "../listar-simbolos/data-service.service";
+import {ConverteMoedasService} from "./converte-moedas.service";
 
-    export interface UserData {
-      id: string;
-      name: string;
-      progress: string;
-      fruit: string;
-    }
-    /** Constants used to fill up our data base. */
-    const FRUITS: string[] = [
-      'blueberry', 'lychee', 'kiwi', 'mango', 'peach', 'lime', 'pomegranate', 'pineapple'
-    ];
-    const NAMES: string[] = [
-      'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
-      'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
-    ];
 
 @Component({
   selector: 'app-converte-moedas',
   templateUrl: './converte-moedas.component.html',
   styleUrls: ['./converte-moedas.component.css']
 })
-export class ConverteMoedasComponent implements AfterViewInit {
+export class ConverteMoedasComponent implements OnInit {
 
-  displayedColumns: string[] = ['id', 'name', 'progress', 'fruit'];
-  dataSource: MatTableDataSource<UserData>;
+  displayedColumns = ['userId', 'id', 'title'];
+  dataSource!:MatTableDataSource<any>;
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild('paginator') paginator! : MatPaginator;
+  @ViewChild(MatSort) matSort! : MatSort;
+  constructor(private service: ConverteMoedasService) {
 
-  constructor() {
-    // Create 100 users
-    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
-
-    // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
   }
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+  ngOnInit(): void {
+    this.service.getUserData().subscribe((response: any) => {
+      this.dataSource = new MatTableDataSource(response)
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.matSort;
+      console.log('response is', response);
+    })
   }
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
+  filterData($event : any){
+    this.dataSource.filter = $event.target.value;
   }
-}
-
-    /** Builds and returns a new User. */
-    function createNewUser(id: number): UserData {
-      const name = NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
-        NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
-
-      return {
-        id: id.toString(),
-        name: name,
-        progress: Math.round(Math.random() * 100).toString(),
-        fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))]
-      };
 }
